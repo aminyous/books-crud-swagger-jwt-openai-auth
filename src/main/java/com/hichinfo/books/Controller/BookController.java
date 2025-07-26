c93025e9d01bc7bb3829d8d17279d6e9bd17bbfb
Amine Hichri <amyn.hichri@gmail.com>
2025-07-26 10:24:24 +0200

    Add PathVariable and RequestParam to the Get method request

--- src/main/java/com/hichinfo/books/Controller/BookController.java
+++ src/main/java/com/hichinfo/books/Controller/BookController.java
@@ -2,6 +2,8 @@ package com.hichinfo.books.Controller;
 
 import com.hichinfo.books.Entities.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -32,4 +34,82 @@ public class BookController {
     public List<Book> getBooks(){
         return books;
     }
+
+
+    @GetMapping("/api/books/forloop/{title}")
+    public Book getBookByTitleForLoop(@PathVariable String title){
+
+        for(Book book: books){
+            if(book.getTitle().equalsIgnoreCase(title)){
+                return book;
+            }
+        }
+        return null;
+
+    }
+
+    @GetMapping("/api/books/{title}")
+    public Book getBookByTitle(@PathVariable String title){
+
+        return books.stream().filter(myBook -> myBook.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+
+    }
+
+    @GetMapping("/api/books/byCatForLoop")
+    public List<Book> getBookByCategoryForLoop(@RequestParam(required = false) String category){
+
+        List<Book> bookListCat = new ArrayList<>();
+
+        if(category == null){
+            return books;
+        }
+
+        for(Book book: books){
+            if(book.getCategory().equalsIgnoreCase(category)){
+                bookListCat.add(book);
+            }
+        }
+
+        return bookListCat;
+    }
+
+    @GetMapping("/api/books/byCat")
+    public List<Book> getBookByCategory(@RequestParam(required = false) String category){
+
+        if(category == null){
+            return books;
+        }
+
+        return books.stream()
+                .filter(myBook -> myBook.getCategory().equalsIgnoreCase(category))
+                .toList();
+
+    }
+
+    @GetMapping("/api/books/byCatTitle/{title}")
+    public Book getBookByCategoryAndTitle(@PathVariable String title,
+                                          @RequestParam(required = false) String category){
+
+
+        if (category != null){
+            List<Book> bookList = books.stream()
+                    .filter(myBook -> myBook.getCategory().equalsIgnoreCase(category))
+                    .toList();
+
+            return bookList.stream()
+                    .filter(myBook -> myBook.getTitle().equalsIgnoreCase(title))
+                    .findFirst()
+                    .orElse(null);
+        }
+
+        return null;
+
+
+
+
+    }
+
+
 }
